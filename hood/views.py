d1c78a41e5b66a973892ef49c54ec627433319f7
CindyKatoni <katmuema@gmail.com>
2020-10-31 18:48:36 +0300

    Basic template structure

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'hood/dashboard.html')
+
+def systemadmin(request):
+    return render(request, 'hood/systemadmin.html')
+
+def neighbourhoodadmin(request):
+    return render(request, 'hood/neighbourhoodadmin.html')   
