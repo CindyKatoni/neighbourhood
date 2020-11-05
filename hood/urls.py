f97a9bed4caa529384153996b1914f4c6554c995
CindyKatoni <katmuema@gmail.com>
2020-11-05 11:14:15 +0300

    registration views

--- hood/urls.py
+++ hood/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='hood-home'),
     path('systemadmin/', views.systemadmin),
     path('neighbourhoodadmin/', views.neighbourhoodadmin),
     path('client/', views.client),
